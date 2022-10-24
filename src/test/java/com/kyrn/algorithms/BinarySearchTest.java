7ab562d5134f05c341b4c47e229f39108cfb5a3f
sametkayiran@gmail.com <sametkayiran@gmail.com>
2022-10-24 20:24:00 +0300

    givenSortedArray_whenGivenItemNotExist_thenReturnNull test has been added.

--- src/test/java/com/kyrn/algorithms/BinarySearchTest.java
+++ src/test/java/com/kyrn/algorithms/BinarySearchTest.java
@@ -15,9 +15,22 @@ public class BinarySearchTest {
         int givenElement = 37;
 
         //when - action or the behaviour that we are going to test
-        int result = binarySearch.search(givenArr, givenElement);
+        Integer result = binarySearch.search(givenArr, givenElement);
 
         //then - verify the output
         assertThat(result).isEqualTo(givenElement);
+    }
+
+    @Test
+    public void givenSortedArray_whenGivenItemNotExist_thenReturnNull() {
+        //given - precondition or setup
+        int[] givenArr = {1, 5, 8, 12, 25, 36, 37, 58, 59, 63, 77, 85, 99};
+        int givenElement = 100;
+
+        //when - action or the behaviour that we are going to test
+        Integer result = binarySearch.search(givenArr, givenElement);
+
+        //then - verify the output
+        assertThat(result).isNull();
     }
 }
