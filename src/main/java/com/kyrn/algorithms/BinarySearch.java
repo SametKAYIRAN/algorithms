061db0553f9cfc6b3bf45591f853cbb285abaa79
sametkayiran@gmail.com <sametkayiran@gmail.com>
2022-10-24 20:30:28 +0300

    refactor

--- src/main/java/com/kyrn/algorithms/BinarySearch.java
+++ src/main/java/com/kyrn/algorithms/BinarySearch.java
@@ -2,13 +2,13 @@ package com.kyrn.algorithms;
 
 public class BinarySearch {
 
-    public Integer search(int[] givenArr, int givenElement) {
+    public static Integer search(int[] givenArr, int givenElement) {
         int lowestIndex = 0;
         int highestIndex = givenArr.length - 1;
         int midIndex;
         int iteration = 0;
         while (lowestIndex <= highestIndex) {
-            System.out.println(++iteration);
+            System.out.println(++iteration + " . iteration");
             midIndex = (lowestIndex + highestIndex) / 2;
             if (givenArr[midIndex] == givenElement) {
                 return givenArr[midIndex];
--- src/test/java/com/kyrn/algorithms/BinarySearchTest.java
+++ src/test/java/com/kyrn/algorithms/BinarySearchTest.java
@@ -6,16 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinarySearchTest {
 
-    private final BinarySearch binarySearch = new BinarySearch();
+    private final int[] givenArr = {1, 5, 8, 12, 25, 36, 37, 58, 59, 63, 77, 85, 99};
 
     @Test
     public void givenSortedArray_whenGivenItemExist_thenReturnGivenItem() {
         //given - precondition or setup
-        int[] givenArr = {1, 5, 8, 12, 25, 36, 37, 58, 59, 63, 77, 85, 99};
         int givenElement = 37;
 
         //when - action or the behaviour that we are going to test
-        Integer result = binarySearch.search(givenArr, givenElement);
+        Integer result = BinarySearch.search(givenArr, givenElement);
 
         //then - verify the output
         assertThat(result).isEqualTo(givenElement);
@@ -24,11 +23,10 @@ public class BinarySearchTest {
     @Test
     public void givenSortedArray_whenGivenItemNotExist_thenReturnNull() {
         //given - precondition or setup
-        int[] givenArr = {1, 5, 8, 12, 25, 36, 37, 58, 59, 63, 77, 85, 99};
         int givenElement = 100;
 
         //when - action or the behaviour that we are going to test
-        Integer result = binarySearch.search(givenArr, givenElement);
+        Integer result = BinarySearch.search(givenArr, givenElement);
 
         //then - verify the output
         assertThat(result).isNull();
