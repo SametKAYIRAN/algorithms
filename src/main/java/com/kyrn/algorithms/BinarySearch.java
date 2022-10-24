b6326b4c284f50c5b294e1c75d660462a61a3003
sametkayiran@gmail.com <sametkayiran@gmail.com>
2022-10-24 19:34:04 +0300

    givenSortedArray_whenSearchedItemExist_thenReturnSearchedItem test has been added

---
+++ src/main/java/com/kyrn/algorithms/BinarySearch.java
@@ -0,0 +1,9 @@
+package com.kyrn.algorithms;
+
+public class BinarySearch {
+
+
+    public int search(int[] givenArr, int givenElement){
+        return 0;
+    }
+}
---
+++ src/test/java/com/kyrn/algorithms/BinarySearchTest.java
@@ -0,0 +1,23 @@
+package com.kyrn.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BinarySearchTest {
+
+    private final BinarySearch binarySearch = new BinarySearch();
+
+    @Test
+    public void givenSortedArray_whenSearchedItemExist_thenReturnSearchedItem() {
+        //given - precondition or setup
+        int[] givenArr = {1, 5, 8, 12, 25, 36, 37, 58, 59, 63, 77, 85, 99};
+        int givenElement = 37;
+
+        //when - action or the behaviour that we are going to test
+        int result = binarySearch.search(givenArr, givenElement);
+
+        //then - verify the output
+        assertThat(result).isEqualTo(givenElement);
+    }
+}
