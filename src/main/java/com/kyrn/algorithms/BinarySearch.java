bedd11c9230b5deb7231c7e71c70cc54daf07a7b
sametkayiran@gmail.com <sametkayiran@gmail.com>
2022-10-24 20:19:22 +0300

    BinarySearch.search(int[] givenArr, int givenElement) method has been added.

--- src/main/java/com/kyrn/algorithms/BinarySearch.java
+++ src/main/java/com/kyrn/algorithms/BinarySearch.java
@@ -2,8 +2,22 @@ package com.kyrn.algorithms;
 
 public class BinarySearch {
 
-
-    public int search(int[] givenArr, int givenElement){
-        return 0;
+    public Integer search(int[] givenArr, int givenElement) {
+        int lowestIndex = 0;
+        int highestIndex = givenArr.length - 1;
+        int midIndex;
+        int iteration = 0;
+        while (lowestIndex <= highestIndex) {
+            System.out.println(++iteration);
+            midIndex = (lowestIndex + highestIndex) / 2;
+            if (givenArr[midIndex] == givenElement) {
+                return givenArr[midIndex];
+            } else if (givenArr[midIndex] > givenElement) {
+                highestIndex = midIndex - 1;
+            } else {
+                lowestIndex = midIndex + 1;
+            }
+        }
+        return null;
     }
 }
